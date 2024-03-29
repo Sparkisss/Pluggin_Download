eaefb1cd4a6f9130aa3c0c0d820de7f637d6e102
Igor Siamykin <sparkisisss@gmail.com>
2024-03-29 22:03:15 +0300

    29-03-24

--- src/modules/APIForecast.ts
+++ src/modules/APIForecast.ts
@@ -1,34 +1,94 @@
 import { apiKey } from "./APIweather"
-// import { getLastWeekDates, addDate } from "./weatherSlide"
+import { getLastWeekDates, addDate } from "./weatherSlide"
 
 const forecastPlace = document.querySelector('.data_now') as HTMLElement
-let selectorDate: string = '2024-03-29'
 
 const apiForecastKeyUrl = 
     `https://api.openweathermap.org/data/2.5/forecast?units=metric&q=brest&appid=${apiKey}`
 
-export async function checkForecastWeather() {
+export async function checkForecastWeather(fileDate:string) {
     const response = await fetch(apiForecastKeyUrl)
     const data = await response.json()
     const forecastDate = data.list
-    console.log(data.list[1])
-    getDayForecast(selectorDate, forecastDate)
+    let ourDate: any = getDayForecast(fileDate, forecastDate)
+    for (const key in ourDate) {
+        if (ourDate.hasOwnProperty(key)) {
+            const value = ourDate[key];
+            addDate(forecastPlace, value, false)
+        }
+    }
 }
 
-function forecastRender() {
-    const d = document.createElement('div');
-    d.textContent = 'd';
-    // Вставляем элемент d после forecastPlace
-    forecastPlace.insertAdjacentElement('afterend', d);
+interface WeatherData {
+    dt_txt: string;
+    main: MainData;
+    wind: MainWind;
+    weather: MainWeather;
+    // Другие свойства, если есть
 }
-
-function getDayForecast(selectorDate: string, arr: string[]) {
-    arr.forEach((item, i) => {
-        if (selectorDate === item) {
-            console.log('successes')
-        }else console.log('fail')
-    }) 
+interface MainData {
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
 }
+interface MainWind {
+    speed: number
+}
+interface MainWeather {
+    0: MainWeatherDescriprion
+}
+interface MainWeatherDescriprion {
+    description: string
+}
+function getDayForecast(selectorDate: string, arr: WeatherData[]) {
 
+    interface SetOfData {
+        temp_morning: number;
+        fells_like_morning: number;
+        temp_afternoon: number;
+        fells_like_afternoon: number;
+        temp_night: number;
+        fells_like_night: number;
+        humidity: number;
+        wind: number;
+        pressure: number;
+        weather: string;
+    }
 
-checkForecastWeather()
+    const setOfData: SetOfData = {
+        temp_morning: 0,
+        fells_like_morning: 0,
+        temp_afternoon: 0,
+        fells_like_afternoon: 0,
+        temp_night: 0,
+        fells_like_night: 0,
+        humidity: 0,
+        wind: 0,
+        pressure: 0,
+        weather: 'cloud'
+    };
+    
+    arr.forEach((arrItem) => {
+        if (typeof arrItem.dt_txt === 'string' && selectorDate.split(' ')[0] === arrItem.dt_txt.split(' ')[0]) {
+            let forecastTime = arrItem.dt_txt.split(' ')[1]
+            if (forecastTime === '09:00:00') {
+                setOfData.temp_morning = arrItem.main.temp
+                setOfData.fells_like_morning = arrItem.main.feels_like                
+            }
+            if (forecastTime === '18:00:00') {
+                setOfData.temp_afternoon = arrItem.main.temp
+                setOfData.fells_like_afternoon = arrItem.main.feels_like
+                setOfData.humidity = arrItem.main.humidity
+                setOfData.pressure = arrItem.main.pressure
+                setOfData.wind = arrItem.wind.speed
+                setOfData.weather = arrItem.weather[0].description            
+            }
+            if (forecastTime === '21:00:00') {
+                setOfData.temp_night = arrItem.main.temp
+                setOfData.fells_like_night = arrItem.main.feels_like                
+            }
+        }
+    });
+    return setOfData
+}
--- src/modules/weatherSlide.ts
+++ src/modules/weatherSlide.ts
@@ -1,9 +1,11 @@
 import {formatDate} from "./clock"
+import {checkForecastWeather} from "./APIForecast"
 
 const articleEquipment = document.querySelector('.error_list') as HTMLElement
 const articleObjectData = document.querySelector('.data_now') as HTMLElement
 const container = document.createElement('div')
 let sliderNumber: number = 0;
+let selectorDate: string = ''
 
 //создаем элементы для отображения дней недели и стрелки для управления
 export function chancheInnerEquipment() {    
@@ -22,14 +24,14 @@ export function chancheInnerEquipment() {
         articleEquipment.innerHTML = ''
         articleEquipment.appendChild(arrowUp)
         articleEquipment.appendChild(container)
-        addDate(container, getLastWeekDates(sliderNumber))
-        addDate(articleObjectData, getLastWeekDates(sliderNumber))
+        addDate(container, getLastWeekDates(sliderNumber), true)
+        addDate(articleObjectData, getLastWeekDates(sliderNumber), true)
         articleEquipment.appendChild(arrowDown)
     }
 }
 //отрисовка новых элементов слайда и удаление старых
-export function addDate(selector: HTMLElement, dates: any): void {
-    selector.innerHTML = ''; // Очищаем содержимое контейнера
+export function addDate(selector: HTMLElement, dates: any, clean: boolean): void {
+    if(clean)selector.innerHTML = ''; // Очищаем содержимое контейнера    
     // Проходим по всем датам в массиве
     let dateDiv: HTMLDivElement = document.createElement('div'); // Создаем новый элемент div для даты
     dateDiv.textContent = dates; // Устанавливаем текстовое содержимое div равным текущей дате
@@ -41,17 +43,19 @@ articleEquipment.addEventListener('click', (e) => {
 
     if (target.alt === 'up') {
         sliderNumber += 1
-        if (sliderNumber >= 5) sliderNumber = 5
-        sessionStorage.setItem('slideNumber', sliderNumber.toString());
-        addDate(container, getLastWeekDates(sliderNumber))
-        addDate(articleObjectData, getLastWeekDates(sliderNumber))
+        if (sliderNumber >= 4) sliderNumber = 4
+        addDate(container, getLastWeekDates(sliderNumber), true)
+        addDate(articleObjectData, getLastWeekDates(sliderNumber), true)
+        selectorDate = articleObjectData.innerText
+        checkForecastWeather(selectorDate)
     }
     if (target.alt === 'down') {
         sliderNumber -= 1
         if (sliderNumber <= 0 ) sliderNumber = 0
-        sessionStorage.setItem('slideNumber', sliderNumber.toString());
-        addDate(container, getLastWeekDates(sliderNumber))
-        addDate(articleObjectData, getLastWeekDates(sliderNumber))
+        addDate(container, getLastWeekDates(sliderNumber), true)
+        addDate(articleObjectData, getLastWeekDates(sliderNumber), true)
+        selectorDate = articleObjectData.innerText
+        checkForecastWeather(selectorDate)
     }
 })
 
@@ -60,7 +64,7 @@ export function getLastWeekDates(num: number) {
     // Копируем текущую дату
     const date = new Date(today);
     // Вычитаем нужное количество дней, чтобы получить дату за прошлую неделю
-    date.setDate(date.getDate() + (num));
+    date.setDate(date.getDate() + (num + 1));
     // Форматируем дату
     return formatDate(date, false)
 }
